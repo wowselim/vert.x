9fe9946a4cf2fe87612bc3791dbe6e7d664fc5e9
Julien Viet <julien@julienviet.com>
2017-01-05 11:09:43 +0100

    Counter fluent future

--- src/main/java/io/vertx/core/shareddata/Counter.java
+++ src/main/java/io/vertx/core/shareddata/Counter.java
@@ -18,6 +18,7 @@ package io.vertx.core.shareddata;
 
 import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 
 /**
@@ -37,12 +38,24 @@ public interface Counter {
    */
   void get(Handler<AsyncResult<Long>> resultHandler);
 
+  default Future<Long> get() {
+    Future<Long> fut = Future.future();
+    get(fut.completer());
+    return fut;
+  }
+
   /**
    * Increment the counter atomically and return the new count
    *
    * @param resultHandler handler which will be passed the value
    */
   void incrementAndGet(Handler<AsyncResult<Long>> resultHandler);
+
+  default Future<Long> incrementAndGet() {
+    Future<Long> fut = Future.future();
+    incrementAndGet(fut.completer());
+    return fut;
+  }
 
   /**
    * Increment the counter atomically and return the value before the increment.
@@ -51,12 +64,24 @@ public interface Counter {
    */
   void getAndIncrement(Handler<AsyncResult<Long>> resultHandler);
 
+  default Future<Long> getAndIncrement() {
+    Future<Long> fut = Future.future();
+    getAndIncrement(fut.completer());
+    return fut;
+  }
+
   /**
    * Decrement the counter atomically and return the new count
    *
    * @param resultHandler handler which will be passed the value
    */
   void decrementAndGet(Handler<AsyncResult<Long>> resultHandler);
+
+  default Future<Long> decrementAndGet() {
+    Future<Long> fut = Future.future();
+    decrementAndGet(fut.completer());
+    return fut;
+  }
 
   /**
    * Add the value to the counter atomically and return the new count
@@ -66,6 +91,12 @@ public interface Counter {
    */
   void addAndGet(long value, Handler<AsyncResult<Long>> resultHandler);
 
+  default Future<Long> addAndGet(long value) {
+    Future<Long> fut = Future.future();
+    addAndGet(value, fut.completer());
+    return fut;
+  }
+
   /**
    * Add the value to the counter atomically and return the value before the add
    *
@@ -73,6 +104,12 @@ public interface Counter {
    * @param resultHandler handler which will be passed the value
    */
   void getAndAdd(long value, Handler<AsyncResult<Long>> resultHandler);
+
+  default Future<Long> getAndAdd(long value) {
+    Future<Long> fut = Future.future();
+    getAndAdd(value, fut.completer());
+    return fut;
+  }
 
   /**
    * Set the counter to the specified value only if the current value is the expectec value. This happens
@@ -83,4 +120,10 @@ public interface Counter {
    * @param resultHandler  the handler will be passed true on success
    */
   void compareAndSet(long expected, long value, Handler<AsyncResult<Boolean>> resultHandler);
+
+  default Future<Boolean> compareAndSet(long expected, long value) {
+    Future<Boolean> fut = Future.future();
+    compareAndSet(expected, value, fut.completer());
+    return fut;
+  }
 }
